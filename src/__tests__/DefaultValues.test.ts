49d7f0690f67d91d2dfd12d6871f5b67ccc16b3d
D34dMan <shibinkidd@gmail.com>
2020-02-19 00:15:16 +0100

    Added addFilter method

--- src/__tests__/DefaultValues.test.ts
+++ src/__tests__/DefaultValues.test.ts
@@ -3,3 +3,17 @@ test('Empty Default Values', () => {
   let api = new DrupalJsonApiParams();
   expect(api.getQueryString()).toBe('');
 });
+
+test('Filter for `status = 1`', () => {
+  let api = new DrupalJsonApiParams();
+  api.addFilter('status', '1');
+  expect(api.getQueryString()).toBe('filter%5Bstatus%5D=1');
+});
+
+test('Filter for `status = 1` && `status = 2`', () => {
+  let api = new DrupalJsonApiParams();
+  api.addFilter('status', '1').addFilter('status', '2');
+  expect(api.getQueryString()).toBe(
+    'filter%5B1%5D%5Bcondition%5D%5Bpath%5D=status&filter%5B1%5D%5Bcondition%5D%5Bvalue%5D=2&filter%5Bstatus%5D=1',
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,63 @@
-import qs = require('qs')
+import qs = require('qs');
+
+interface FilterItems {
+  [key: string]: FilterItem | string;
+}
+
+interface FilterItem {
+  condition: {
+    operator?: string;
+    group?: string;
+    path: string;
+    value: string;
+  };
+}
 
 export class DrupalJsonApiParams {
- 
-  private filter:Object = {};
-  private sort:Object = {};
-  private fields:Object = {};
-  private page:Object = {};
+  private filter: FilterItems = {};
+  private sort: Object = {};
+  private fields: Object = {};
+  private page: Object = {};
 
+  public addFilter(
+    path: string,
+    value: string,
+    operator: string = '=',
+    group: string | undefined = undefined,
+  ): DrupalJsonApiParams {
+    if (operator === '=' && group === undefined && this.filter[path] === undefined) {
+      this.filter[path] = value;
+      return this;
+    }
 
-  getQueryString ():string {
-    let data = {}
-    return qs.stringify(data)
+    var name = this.getIndexId(this.filter, path);
+
+    this.filter[name] = {
+      condition: {
+        path: path,
+        value: value,
+        ...(operator !== '=' && { operator: operator }),
+        ...(group !== undefined && { group: group }),
+      },
+    };
+
+    return this;
   }
 
+  getIndexId(obj: any, proposedKey: string): string {
+    let key: string;
+    if (obj[proposedKey] === undefined) {
+      key = proposedKey;
+    } else {
+      key = Object.keys(obj).length.toString();
+    }
+    return key;
+  }
+
+  public getQueryString(): string {
+    let data = {
+      filter: this.filter,
+    };
+    return qs.stringify(data);
+  }
 }
