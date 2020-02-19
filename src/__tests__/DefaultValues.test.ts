a4b1888aa698ba17281ecd57405a3a3d2ecc707a
D34dMan <shibinkidd@gmail.com>
2020-02-19 22:07:49 +0100

    Add test for group and conditions for filter

--- src/__tests__/DefaultValues.test.ts
+++ src/__tests__/DefaultValues.test.ts
@@ -17,3 +17,14 @@ test('Filter for `status = 1` && `status = 2`', () => {
     'filter%5B1%5D%5Bcondition%5D%5Bpath%5D=status&filter%5B1%5D%5Bcondition%5D%5Bvalue%5D=2&filter%5Bstatus%5D=1',
   );
 });
+
+
+test('Filter for `status = 1` && `status != 2` in group=publish_status', () => {
+  let api = new DrupalJsonApiParams();
+  api
+    .addFilter('status', '1', '=', 'publish_status')
+    .addFilter('status', '2', '!=', 'publish_status');
+  expect(api.getQueryString()).toBe(
+    'filter%5B1%5D%5Bcondition%5D%5Bpath%5D=status&filter%5B1%5D%5Bcondition%5D%5Bvalue%5D=2&filter%5B1%5D%5Bcondition%5D%5Boperator%5D=%21%3D&filter%5B1%5D%5Bcondition%5D%5Bgroup%5D=publish_status&filter%5Bstatus%5D%5Bcondition%5D%5Bpath%5D=status&filter%5Bstatus%5D%5Bcondition%5D%5Bvalue%5D=1&filter%5Bstatus%5D%5Bcondition%5D%5Bgroup%5D=publish_status',
+  );
+});
